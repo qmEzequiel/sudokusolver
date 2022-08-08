8484ad48d7a4412eda2b8a292ffe1efa9773a9e4
qmEzequiel <tabassocarlo@gmail.com>
2022-08-08 13:43:50 +0200

    applied 3d matrix idea

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-void read_matrix(FILE *fp, int m[9][9]);
-void print_matrix(int m[9][9]);
-void solve_matrix(int m[9][9]);
+void read_matrix(FILE *fp, bool m[9][9][9]);
+void print_matrix(bool m[9][9][9]);
+void solve_matrix(bool m[9][9][9]);
 
 int main(void){
     FILE *fp;
-    int m[9][9];
+    bool m[9][9][9];
     //open the sample file with the sudoku to solve
     fp = fopen("sample.txt", "r");
     if (fp == NULL)
@@ -21,41 +22,52 @@ int main(void){
     //print the matrix with the sudoku unsolved
     print_matrix(m);
     //solve the sudoku
-    solve_matrix(m);
+    //solve_matrix(m);
     //print the matrix with the sudoku solved 
-    print_matrix(m);
+    //print_matrix(m);
 
     return 0;
 }
 
-void read_matrix(FILE *fp, int m[9][9]){
+void read_matrix(FILE *fp, bool m[9][9][9]){
     int n;
     for(int i=0; i<9; i++){
         for(int j=0; j<9; j++){
             fscanf(fp, "%d", &n);
-            m[i][j] = n;
+            for(int z=1; z<=9; z++){
+                if(n==0) m[i][j][z-1]=false;
+                else {
+                    if(n==z) m[i][j][z-1]=true;
+                    else m[i][j][z-1]=false;
+                }
+            }
         }
     }
 }
-void print_matrix(int m[9][9]){
+void print_matrix(bool m[9][9][9]){
+    int n;
     for(int i=0; i<9; i++){
         for(int j=0; j<9; j++){
-            if(m[i][j] == 0) printf(" ");
-            else printf("%d", m[i][j]);
+            for(int z=1; z<=9; z++){
+                if(m[i][j][z-1]) {n=z; printf("%d", z);}
+                else {n=0;}
+            }
+            // if(n == 0) printf(" ");
+            // else printf("%d", n);
             printf(" ");
         }
         printf("\n");
     }
 }
 
-void solve_matrix(int m[9][9]){
-    int a={1,2,3,4,5,6,7,8,9};
-    for(int i=0; i<9; i++){
-        for(int j=0; j<9; j++){
-            for(int z=0; z<9; z++){
-                printf("-");
-            }
-        }
-    }
-    printf("\n");
-}
+// void solve_matrix(bool m[9][9][9]){
+//     int a={1,2,3,4,5,6,7,8,9};
+//     for(int i=0; i<9; i++){
+//         for(int j=0; j<9; j++){
+//             for(int z=0; z<9; z++){
+//                 printf("-");
+//             }
+//         }
+//     }
+//     printf("\n");
+// }
