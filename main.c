c324911afd50359f829c9d4f7619c4c3cf1c51d7
qmEzequiel <tabassocarlo@gmail.com>
2022-09-06 00:30:05 +0200

    normal approach initial implementation

--- main.c
+++ main.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void read_matrix(FILE *fp, bool m[9][9][9]);
-void print_matrix(bool m[9][9][9]);
-void solve_matrix(bool m[9][9][9]);
-bool nine_logic_xor(bool v[9]);
-bool rc_control(bool r[9][9], int value);
-bool square_control(bool sq[3][3][9]);
+void read_matrix(FILE *fp, int m[9][9]);
+void print_matrix(int m[9][9]);
+void solve_matrix(int m[9][9]);
+bool row_control(int r[9], int value);
+bool column_control(int m[9][9], int c, int value);
+bool square_control(int m[9][9], int r, int c, int value);
 
 int main(void){
     FILE *fp;
-    bool m[9][9][9];
+    int m[9][9];
     //open the sample file with the sudoku to solve
     fp = fopen("sample.txt", "r");
     if (fp == NULL)
@@ -25,38 +25,26 @@ int main(void){
     //print the matrix with the sudoku unsolved
     print_matrix(m);
     //solve the sudoku
-    //solve_matrix(m);
+    solve_matrix(m);
     //print the matrix with the sudoku solved 
-    //print_matrix(m);
+    printf("\n");
+    print_matrix(m);
 
     return 0;
 }
 
-void read_matrix(FILE *fp, bool m[9][9][9]){
-    int n;
+void read_matrix(FILE *fp, int m[9][9]){
     for(int i=0; i<9; i++){
         for(int j=0; j<9; j++){
-            fscanf(fp, "%d", &n);
-            for(int z=1; z<=9; z++){
-                if(n==0) m[i][j][z-1]=false;
-                else {
-                    if(n==z) m[i][j][z-1]=true;
-                    else m[i][j][z-1]=false;
-                }
-            }
+            fscanf(fp, "%d", &m[i][j]);
         }
     }
 }
-void print_matrix(bool m[9][9][9]){
-    int n;
+void print_matrix(int m[9][9]){
     for(int i=0; i<9; i++){
         for(int j=0; j<9; j++){
-            for(int z=1; z<=9; z++){
-                if(m[i][j][z-1]) { n=z; break;}
-                else {n=0;}
-            }
-            if(n == 0) printf("  ");
-            else printf("%d ", n);
+            if(m[i][j] == 0) printf("  ");
+            else printf("%d ", m[i][j]);
             if(j==2 || j==5) printf("|");
         }
         printf("\n");
@@ -64,30 +52,38 @@ void print_matrix(bool m[9][9][9]){
     }
 }
 
-void solve_matrix(bool m[9][9][9]){
+void solve_matrix(int m[9][9]){
     for(int i=0; i<9; i++){
         for(int j=0; j<9; j++){
-            if(!nine_logic_xor(m[i][j])){
-                for(int z=0; z<9; z++){
-                    //if(rc_control(m[i], z) and rc_control(m[][i], z)) m[i][j][z]=true;
+            int n=0, a=0;
+            if(m[i][j] == 0){
+                for(int z=1; z<=9; z++){
+                    if(row_control(m[i], z) && column_control(m, j, z) && square_control(m,i,j,z)) {
+                        a=z;
+                        n++;
+                    }
                 }
+                if(n==1) m[i][j]=a;
             }
         }
     }
 }
 
-bool nine_logic_xor(bool v[9]){
-    int n=0;
-    for(int i=0; i<9; i++){
-        if(v[i]) n++;
-    }
-    if(n==1) return true;
-    return false;
-}
-
-bool rc_control(bool r[9][9], int value){
+bool row_control(int r[9], int value){
     for(int i=0; i<9; i++)
-        if(r[i][value]) return false;
+        if(r[i]==value) return false;
     return true;
 }
-bool square_control(bool sq[3][3][9]);
+bool column_control(int m[9][9], int c, int value){
+    for(int i=0; i<9; i++)
+        if(m[c][i]==value) return false;
+    return true;
+}
+bool square_control(int m[9][9], int r, int c, int value){
+    for(int i=r/3; i<r/3+3; i++){
+        for(int j=c/3; j<c/3+3; j++){
+            if(m[i][j]==value) return false;
+        }
+    }
+    return true;
+}
